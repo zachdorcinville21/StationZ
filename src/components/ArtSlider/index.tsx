9b5a857de78e85c44fad09a2aef625d0e0cbf744
Zachary Dorcinville <zdorcinville@gmail.com>
2021-10-16 20:51:49 -0400

    Added new nfts

--- src/components/ArtSlider/index.tsx
+++ src/components/ArtSlider/index.tsx
@@ -3,7 +3,9 @@ import { ThemeContext } from "../../context/theme";
 import { SliderContainer } from "./styles";
 import ravager from '../../assets/images/ravager.png';
 import roamer from '../../assets/images/roamer.png';
-import popsicle from '../../assets/images/popsicles.jpg';
+import belugie171 from '../../assets/images/belugie 171.png';
+import belugie628 from '../../assets/images/belugie 628.png';
+import pixsol from '../../assets/images/pixsol.png';
 import Carousel, { autoplayPlugin, slidesToShowPlugin } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
@@ -33,7 +35,9 @@ export const ArtSlider = () => {
             >
                 <img className='art-piece' src={ravager} style={theme.artStyle} alt='art' />
                 <img className='art-piece' src={roamer} style={theme.artStyle} alt='art' />
-                <img className='art-piece' src={popsicle} style={theme.artStyle} alt='art' />
+                <img className='art-piece' src={belugie171} style={theme.artStyle} alt='art' />
+                <img className='art-piece' src={pixsol} style={theme.artStyle} alt='art' />
+                <img className='art-piece' src={belugie628} style={theme.artStyle} alt='art' />
             </Carousel>
         </SliderContainer>
     );
