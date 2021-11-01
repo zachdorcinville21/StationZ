e5a3c2893af8d3815349355fde04a7952d0886fe
Zachary Dorcinville <zdorcinville@gmail.com>
2021-10-31 21:02:55 -0400

    New nfts added

--- src/components/ArtSlider/index.tsx
+++ src/components/ArtSlider/index.tsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import { ThemeContext } from "../../context/theme";
 import { SliderContainer } from "./styles";
 import ravager from '../../assets/images/ravager.png';
+import ravager343 from '../../assets/images/ravager343.png';
 import roamer from '../../assets/images/roamer.png';
 import belugie171 from '../../assets/images/belugie 171.png';
 import belugie628 from '../../assets/images/belugie 628.png';
-import pixsol from '../../assets/images/pixsol.png';
+import babyape from '../../assets/images/babyape1764.png';
 import Carousel, { autoplayPlugin, slidesToShowPlugin } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
@@ -33,11 +34,12 @@ export const ArtSlider = () => {
                 ]}
                 animationSpeed={1000}
             >
-                <img className='art-piece' src={ravager} style={theme.artStyle} alt='art' />
+                <img className='art-piece' src={babyape} style={theme.artStyle} alt='art' />
                 <img className='art-piece' src={roamer} style={theme.artStyle} alt='art' />
                 <img className='art-piece' src={belugie171} style={theme.artStyle} alt='art' />
-                <img className='art-piece' src={pixsol} style={theme.artStyle} alt='art' />
+                <img className='art-piece' src={ravager} style={theme.artStyle} alt='art' />
                 <img className='art-piece' src={belugie628} style={theme.artStyle} alt='art' />
+                <img className='art-piece' src={ravager343} style={theme.artStyle} alt='art' />
             </Carousel>
         </SliderContainer>
     );
